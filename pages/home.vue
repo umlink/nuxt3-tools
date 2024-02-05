5304eb181ac11a1c7349888baee8bb62fb14fd1c
umlink <link_0403@163.com>
2024-02-05 17:41:06 +0800

    feat: init

---
+++ pages/home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <ClientOnly fallback-tag="span" fallback="Loading comments...">
+      <QuillEditor />
+    </ClientOnly>
+    <NuxtWelcome />
+  </div>
+</template>
+<script setup>
+import { QuillEditor } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css';
+</script>
