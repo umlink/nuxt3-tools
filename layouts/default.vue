5c2e151ba65c10bef7d7e239028c7631407b6bed
umlink <link_0403@163.com>
2024-05-24 15:22:41 +0800

    feat: update

---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <p>一些在所有页面之间共享的默认布局内容</p>
+    <slot />
+  </div>
+</template>
--- pages/home.vue
+++ pages/home.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
+    <Header />
     <div class="bg-green p-4"></div>
     <ClientOnly fallback-tag="span" fallback="Loading comments...">
       <QuillEditor />
     </ClientOnly>
-    <NuxtWelcome />
   </div>
 </template>
 <script setup>
 import { QuillEditor } from '@vueup/vue-quill'
 import '@vueup/vue-quill/dist/vue-quill.snow.css';
+import Header from "~/components/Header.vue";
 </script>
