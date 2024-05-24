683de04715f6b65e0157016d439c5ec60f9b52fc
umlink <link_0403@163.com>
2024-05-24 17:06:11 +0800

    eslint

--- pages/home.vue
+++ pages/home.vue
@@ -1,24 +1,23 @@
 <script setup lang="ts">
-import randomEntry from "../utils/randomEntry";
+  import randomEntry from '../utils/randomEntry';
+  const foo = useFoo();
+  const appConfig = useAppConfig();
+  const runtimeConfig = useRuntimeConfig();
 
-const foo = useFoo()
-const appConfig = useAppConfig();
-const runtimeConfig = useRuntimeConfig()
-
-const { data } = await useFetch('/api/hello')
+  const { data } = await useFetch('/api/hello');
 </script>
 
 <template>
   <div>
     <Header />
-    {{foo.name}}
-    <div>{{ data}}</div>
-    <div>{{ appConfig.foo}}</div>
-    <div>{{runtimeConfig.apiSecret}}</div>
-    <div>{{randomEntry([1, 2, 3, 4])}}</div>
+    {{ foo.name }}
+    <div>{{ data }}</div>
+    <div>{{ appConfig.foo }}</div>
+    <div>{{ runtimeConfig.apiSecret }}</div>
+    <div>{{ randomEntry([1, 2, 3, 4]) }}</div>
     <div class="bg-zinc-200 rounded-full p-4"></div>
     <main>
-      <ContentDoc path="/introduce/home"/>
+      <ContentDoc path="/introduce/home" />
     </main>
   </div>
 </template>
